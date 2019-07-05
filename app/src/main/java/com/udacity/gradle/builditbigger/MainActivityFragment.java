e75a7f0eaf41494f48dc9cb84e5dbe56a44772c2
Mayada <mayada.abdeen234@gmail.com>
2019-07-05 03:29:59 +0200

    changes

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -2,13 +2,13 @@ package com.udacity.gradle.builditbigger;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
 
-import androidx.fragment.app.Fragment;
 
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
