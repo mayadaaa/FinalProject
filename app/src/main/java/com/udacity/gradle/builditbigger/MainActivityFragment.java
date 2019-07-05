f769bab83c06c764b3c35823f5352ec1b1e54464
Mayada <mayada.abdeen234@gmail.com>
2019-07-05 03:21:55 +0200

    depe

---
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -0,0 +1,57 @@
+package com.udacity.gradle.builditbigger;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+import androidx.fragment.app.Fragment;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    private Unbinder mUnBinder;
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+        mUnBinder = ButterKnife.bind(this, root);
+
+        Button button = ButterKnife.findById(root, R.id.button_show_joke);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                new RetreiveJokes(new RetreiveJokes.Listener() {
+
+                    @Override
+                    public void onJokeLoaded(String joke) {
+                        Intent intent = new Intent(getContext(), JokeViewerActivity.class);
+                        intent.putExtra(JokeViewerActivity.EXTRA_JOKE, joke);
+                        startActivity(intent);
+                    }
+                }).execute();
+            }
+        });
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mUnBinder.unbind();
+    }
+}
---
+++ java_library/src/main/java/com/example/java_library/MyClass.java
@@ -0,0 +1,10 @@
+package com.example.java_library;
+
+public class MyClass  {
+    public static String randomJoke() {
+        return "Two bytes meet. The first byte asks, “Are you ill?”\n" +
+                "\n" +
+                "The second byte replies, “No, just feeling a bit off.”";
+    }
+
+}
